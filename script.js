f571123257e71d6afd6e3d6dbd3b5238f549a98c
jdmlopes <jdlopes97@gmail.com>
2023-02-25 13:44:07 -0300

    Finished the console version of the game

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 let gonAttack = "";
 let pitouAttack = "";
+let gonHealth = 3;
+let pitouHealth = 3;
 
-console.log("Help Gon Defeat Pitou")
+console.log("Help Gon Defeat Pitou");
+console.log("Gon Health: " + gonHealth);
+console.log("Pitou Health: " + pitouHealth);
 
-document.getElementById('play-round').addEventListener("click", function() {
+
+//
+document.getElementById('play-round').addEventListener("click", () => {
     //Player Choice
     gonAttack = validatePlayerInput(prompt("Type the attack you want to perform ('rock', 'Paper', 'Scissors')"));
     //NPC choice
@@ -13,10 +19,33 @@ document.getElementById('play-round').addEventListener("click", function() {
     console.log("Gon Plays: " + gonAttack);
     console.log("Pitou Plays: " + pitouAttack);
     console.log(playRound(gonAttack, pitouAttack));
+    console.log("Gon Health: " + gonHealth);
+    console.log("Pitou Health: " + pitouHealth);
+
+    checkWinner();
+
+    
 
 });
 
 
+function checkWinner(){
+    if(pitouHealth  === 0){
+        console.log("Gon won the fight, Neferpitou was defeated");
+        resetGame();
+    }
+
+    if(gonHealth  === 0){
+        console.log("Neferpitou won the fight, Gon was defeated");
+        resetGame();
+    }
+}
+
+
+function resetGame(){
+    gonHealth = 3;
+    pitouHealth = 3;
+}
 
 function getPitouAttack(){
     let randomAttack = Math.floor(Math.random() * 3) + 1;
@@ -30,33 +59,40 @@ function getPitouAttack(){
     return "error";
 }
 
+//Calculate the result of the round, decreases the health of the looser by 1 and returns a string with the result
 function playRound(player, npc){
     if(player === npc){
         return  "Tie";
     }else if(player === "rock"){
 
         if(npc === "scissors"){
+            pitouHealth--;
             return "Gon Wins";
         }
 
         if(npc === "paper"){
+            gonHealth--;
             return "Pitou Wins";
         }
 
     }else if(player === "paper"){
         if(npc === "rock"){
+            pitouHealth--;
             return "Gon Wins";
         }
         
         if(npc === "scissors"){
+            gonHealth--;
             return "Pitou Wins";
         }
     }else if(player === "scissors"){
         if(npc === "paper"){
+            pitouHealth--;
             return "Gon Wins";
         }
         
         if(npc === "rock"){
+            gonHealth--;
             return "Pitou Wins";
         }
     }
